ffa0e95678bce13c002c0af2477a7fc2bce20940
Adon Rosen <arosen@chead.uphs.upenn.edu>
2016-07-27 16:00:46 -0400

    afgr uploaded matlab function to create a 3d sphere in a nifti image

---
+++ matlab/createNiftiSphere.m
@@ -0,0 +1,41 @@
+function [ circle ] = createNiftiSphere( imageToMask, volumeToUse , centerOfSphere)
+%CreateNiftiCricle is a function which wil be used to create a circle in
+%the input images voxel dimensions with the volume to use as the volume of
+%the sphere this will only create a 3d sphere
+
+% First thing is we have to get the nifti's demensions
+
+
+% Now lets draw a sphere with the correct volume
+% Now lets draw a sphere with the correct volume
+S=zeros(imageToMask.dim(2),imageToMask.dim(3),imageToMask.dim(4));
+xLim=imageToMask.dim(2);
+yLim=imageToMask.dim(3);
+zLim=imageToMask.dim(4);
+% Now find the radius
+radius=((volumeToUse/pi)*(3/4))^1/3;
+% Now lets declare anything we need to do for the center of the sphere
+I = centerOfSphere(1);
+J = centerOfSphere(2);
+K = centerOfSphere(3);
+% Find any differences between center and image center
+% First find the center of the image
+xCent = xLim / 2;
+yCent = yLim / 2;
+zCent = zLim / 2;
+
+% Now find the difference between the  requested center and the image's
+% center
+xTrans = (xCent - I) * 2;
+yTrans = (yCent - J) * 2;
+zTrans = (zCent - K) * 2;
+
+
+for i=1:xLim;for j=1:yLim;for k=1:zLim
+  S(i,j,k)=(i-(xLim+1 - xTrans)/2)^2+(j-(yLim+1 + yTrans)/2)^2+(k-(zLim+1 + zTrans)/2)^2<=radius^2;
+  %or <radius^2
+end;end;end
+
+circle = S;
+end
+
