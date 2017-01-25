0a19b5edaa2a2e4df5cc3bdf0ff5fee39d5b3893
Adon Rosen <arosen@chead.uphs.upenn.edu>
2017-01-25 10:15:01 -0500

    updated to now work with files w/o headers

--- bash/mergeCSV.sh
+++ bash/mergeCSV.sh
@@ -11,3 +11,19 @@ for filename in ./*.csv; do
    i=$(( $i + 1 ))                        # Increase the counter
  fi
 done
+
+# Now fix it so if thre is no header then I will just cat the files 
+if [[ $1 -eq 1 ]] ; then 
+  i=1
+  rm -f ${OutFileName}
+  for filename in ./*.csv; do 
+   if [ "$filename"  != "$OutName" ] ;      # Avoid recursion 
+   then 
+     if [[ $i -eq 0 ]] ; then 
+        head -1  $filename >   $OutFileName # Copy header if it is the first file
+     fi
+     cat  $filename >>  $OutFileName # Append from the 2nd line each file
+     i=$(( $i + 1 ))                        # Increase the counter
+   fi
+  done
+fi
