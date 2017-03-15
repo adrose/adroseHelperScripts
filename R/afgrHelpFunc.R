ddb809cb0fe82acd2db9a3aa5f2562aa8af33404
Adon Rosen <adrose@galton.uphs.upenn.edu>
2017-03-15 17:39:25 -0400

    Added function to compute minimum r needed for sig value given smaple size

--- R/afgrHelpFunc.R
+++ R/afgrHelpFunc.R
@@ -242,3 +242,34 @@ multi.which <- function(A){
     })
 
 }()
+
+# Create a fucntion which will find the minimum value needed for a signifiacnt
+# correlation given a sample size
+getRVal <- function(cohortSize){
+  vals <- seq(.0001,1,.0001)
+  index <- 1
+  pVal <- 1
+  while(pVal > .05) {
+      i <- vals[index]
+      index <- index + 1
+      n     <- cohortSize            # length of vector
+      rho   <- i                     # desired correlation = cos(angle)
+      theta <- acos(rho)             # corresponding angle
+      x1    <- rnorm(n, 1, 1)        # fixed given data
+      x2    <- rnorm(n, 2, 0.5)      # new random data
+      X     <- cbind(x1, x2)         # matrix
+      Xctr  <- scale(X, center=TRUE, scale=FALSE)   # centered columns (mean 0)
+    
+      Id   <- diag(n)                               # identity matrix
+      Q    <- qr.Q(qr(Xctr[ , 1, drop=FALSE]))      # QR-decomposition, just matrix Q
+      P    <- tcrossprod(Q)          # = Q Q'       # projection onto space defined by x1
+      x2o  <- (Id-P) %*% Xctr[ , 2]                 # x2ctr made orthogonal to x1ctr
+      Xc2  <- cbind(Xctr[ , 1], x2o)                # bind to matrix
+      Y    <- Xc2 %*% diag(1/sqrt(colSums(Xc2^2)))  # scale columns to length 1
+    
+      x <- Y[ , 2] + (1 / tan(theta)) * Y[ , 1]     # final new vector
+      cor(x1, x)  # check correlation = rho
+      pVal <- cor.test(x1, x)$p.value
+  }
+  return(i)
+}
